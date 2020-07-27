dc7b321e083cc03555b99acb1ceafbc514a531bd
azimmomin <3822259+azimmomin@users.noreply.github.com>
2020-07-27 13:30:35 -0700

    Finished lesson 3.1.

---
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 10f;
+
+    private Rigidbody playerRigidbody = null;
+    private bool isOnGround = true;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isOnGround = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isOnGround = false;
+    }
+}
---
+++ Prototype3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToSpawn;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float startDelay = 1f;
+    [SerializeField] private float spawnInterval = 3f;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, spawnInterval);
+    }
+
+    private void SpawnObstacle()
+    {
+        Instantiate(objectToSpawn, spawnPoint.position, objectToSpawn.transform.rotation);
+    }
+}
