17f7f28825c11e189079c1bcc660a7c807567da0
azimmomin <3822259+azimmomin@users.noreply.github.com>
2020-07-28 08:30:05 -0700

    Completed exercise 3.3

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -6,23 +6,27 @@ public class PlayerController : MonoBehaviour
     public static Action OnGameOver;
 
     [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float gravityModifier = 1.5f;
 
     private Rigidbody playerRigidbody = null;
+    private Animator playerAnimator = null;
     private bool isOnGround = true;
     private bool isGameOver = false;
 
     private void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
     }
 
     private void Update()
     {
-        if (isGameOver)
-            return;
-
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !isGameOver)
+        {
             playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnimator.SetTrigger("Jump_trig");
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -34,7 +38,8 @@ public class PlayerController : MonoBehaviour
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             isGameOver = true;
-            Debug.Log("Game Over!");
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
             OnGameOver?.Invoke();
         }
     }
